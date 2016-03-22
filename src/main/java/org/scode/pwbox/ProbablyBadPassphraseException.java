f5d7437e6ad1a80ecf0ad41bacd75a462feaf0ef
Peter Schuller <peter.schuller@infidyne.com>
2016-03-21 19:37:53 -0700

    Minor documentation improvement.

--- src/main/java/org/scode/pwbox/ProbablyBadPassphraseException.java
+++ src/main/java/org/scode/pwbox/ProbablyBadPassphraseException.java
@@ -4,8 +4,8 @@ package org.scode.pwbox;
  * Thrown to indicate a decryption attempt failed, and the reason is probably that an incorrect passphrase
  * was specified.
  *
- * This check can never be certain, since any problem is consistent with malicious tampering or arbitrary corruption.
- * This is why it is called "Probably".
+ * We say "probably" because any problem is consistent with malicious tampering or arbitrary corruption. Such
+ * tampering or corruption may under the right circumstances cause this exception to the thrown.
  *
  * In the event that the file has *not* been tampered with however, and it has not been corrupted, this exception
  * will reliably (barring extremely unlikely collissions in key generation) be thrown rather than a different exception
