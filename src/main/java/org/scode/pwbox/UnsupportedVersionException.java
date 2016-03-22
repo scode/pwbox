0ca0ad859b1010ac44ad7cf116fbf07cbc630c5d
Peter Schuller <peter.schuller@infidyne.com>
2016-03-21 19:39:42 -0700

    Another minor documentation improvement.

--- src/main/java/org/scode/pwbox/UnsupportedVersionException.java
+++ src/main/java/org/scode/pwbox/UnsupportedVersionException.java
@@ -2,7 +2,8 @@ package org.scode.pwbox;
 
 /**
  * Indicates PWBox was given encrypted data that seems to be in PWBox format, but a version of the format
- * not supported by the implementation.
+ * not supported by the implementation. This is intended to convey that data has been encrypted with a
+ * newer version of the code than is trying to decrypt it.
  *
  * Note that malicious tampering or arbitrary corruption can trigger this exception.
  */
