c034c09eb69c0502c7be141732309e1fedf6a26b
Peter Schuller <peter.schuller@infidyne.com>
2016-04-05 21:44:38 -0700

    Add "lenient hex" implementation.
    The intent is to go for a very human readable+speakable representation
    that's also simple (hex, not base64) and suitable for
    e.g. cut'n'pasting into whitespace-lossy programs.

--- src/main/java/org/scode/pwbox/util/HexUtil.java
+++ src/main/java/org/scode/pwbox/util/HexUtil.java
@@ -32,6 +32,43 @@ public class HexUtil {
         return ret;
     }
 
+    public static String toLenientHex(String hex) {
+        if (hex.length() % 2 != 0) {
+            throw new IllegalArgumentException("hex string not of size divisible by 2");
+        }
+
+        final StringBuilder ret = new StringBuilder();
+
+        for (int i = 0; i < hex.length(); i++) {
+            if (i > 0) {
+                if (i % 64 == 0) {
+                    ret.append('\n');
+                } else if (i % 4 == 0) {
+                    ret.append(' ');
+                }
+            }
+            ret.append(hex.charAt(i));
+        }
+
+        ret.append('\n');
+
+        return ret.toString();
+    }
+
+    public static String fromLenientHex(String lenientHex) {
+        final StringBuilder ret = new StringBuilder();
+
+        for (int i = 0; i < lenientHex.length(); i++) {
+            final char ch = lenientHex.charAt(i);
+
+            if (!Character.isWhitespace(ch)) {
+                ret.append(ch);
+            }
+        }
+
+        return ret.toString();
+    }
+
     private static int hexChar(char ch) throws InvalidHexException {
         final int ret = Character.digit(ch, 16);
 
--- src/test/java/org/scode/pwbox/util/HexUtilTest.java
+++ src/test/java/org/scode/pwbox/util/HexUtilTest.java
@@ -60,4 +60,21 @@ public class HexUtilTest {
         Assert.assertEquals(HexUtil.toHex(new byte[]{(byte)128}), "80");
         Assert.assertEquals(HexUtil.toHex(new byte[]{(byte)255}), "ff");
     }
+
+    @Test
+    public void fromLenientHex() {
+        Assert.assertEquals(HexUtil.fromLenientHex(""), "");
+        Assert.assertEquals(HexUtil.fromLenientHex("01"), "01");
+        Assert.assertEquals(HexUtil.fromLenientHex("0102 0304\n0506\n\n  07"), "01020304050607");
+    }
+
+    @Test
+    public void toLenientHex() {
+        Assert.assertEquals(HexUtil.toLenientHex(""), "\n");
+        Assert.assertEquals(HexUtil.toLenientHex(
+                "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20" +
+                "2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f40"),
+                "0102 0304 0506 0708 090a 0b0c 0d0e 0f10 1112 1314 1516 1718 191a 1b1c 1d1e 1f20\n" +
+                "2122 2324 2526 2728 292a 2b2c 2d2e 2f30 3132 3334 3536 3738 393a 3b3c 3d3e 3f40\n");
+    }
 }
